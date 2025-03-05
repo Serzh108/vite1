9ca5e63df453f4f22f052c0844910afabe138ab0
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2025-03-05 12:45:57 +0200

    ADD | added snippet for new components

---
+++ src/T.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import styles from './tt.module.css';
+
+export interface IttProps {
+  //
+}
+
+const tt: FC<IttProps> = props => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default tt;
